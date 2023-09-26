ff8e3e6941ee93afc3ac5821babedd2121ae6f4d
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-09-26 16:44:21 -0700

    fixed number of created divs to 256

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let container = document.getElementById('container');
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     newDiv = document.createElement('div');
     newDiv.classList.add('grid');
     container.appendChild(newDiv);
