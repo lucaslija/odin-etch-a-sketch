ecd538836a62b9800e52a059f40dfe9f36349cfd
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-09-26 16:41:39 -0700

    Added class grid to generated divs

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let container = document.getElementById("container");
+let container = document.getElementById('container');
 
 for (let i = 0; i < 16; i++) {
-    newDiv = document.createElement("div");
+    newDiv = document.createElement('div');
+    newDiv.classList.add('grid');
     container.appendChild(newDiv);
 }
