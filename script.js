fe3eaa86cc893f29af4c72ade5189670eec9b4ca
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-09-26 18:17:54 -0700

    Refactored gridGen to account for changing grid size

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let container = document.getElementById('container');
+const container = document.getElementById('container');
+const button = document.querySelector('button');
 
-function gridGen(n = 256) {
-    for (let i = 0; i < n; i++) {
+function gridGen(n = 16) {
+    let gridList = document.querySelectorAll('.grid');
+    gridList.forEach( (node) => {
+        node.parentNode.removeChild(node);
+    })
+    for (let i = 0; i < n * n; i++) {
         newDiv = document.createElement('div');
         newDiv.classList.add('grid');
         container.appendChild(newDiv);
+
+        newDiv.style.width = `calc(90vh / ${n})`;
+        newDiv.style.height = `calc(90vh / ${n})`;
     
         newDiv.addEventListener('mouseover', (event) => {
             event.target.style.backgroundColor = "aqua";
@@ -13,3 +21,11 @@ function gridGen(n = 256) {
 }
 
 gridGen();
+
+button.addEventListener('click', () => {
+    let size = prompt('What size grid would you like?')
+    while (!(size > 0) || !(size <= 100)) {
+        size = prompt('Please input a positive integer less than 100.');
+    };
+    gridGen(size);
+})
