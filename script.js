4dd9bf02d3b73faa7bf8c241f1ffd32f2a04e505
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-09-26 17:46:40 -0700

    Encapsulated grid generation as a function

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let container = document.getElementById('container');
 
-for (let i = 0; i < 256; i++) {
-    newDiv = document.createElement('div');
-    newDiv.classList.add('grid');
-    container.appendChild(newDiv);
-
-    newDiv.addEventListener('mouseover', (event) => {
-        event.target.style.backgroundColor = "aqua";
-    });
+function gridGen(n = 256) {
+    for (let i = 0; i < n; i++) {
+        newDiv = document.createElement('div');
+        newDiv.classList.add('grid');
+        container.appendChild(newDiv);
+    
+        newDiv.addEventListener('mouseover', (event) => {
+            event.target.style.backgroundColor = "aqua";
+        });
+    }
 }
+
+gridGen();
