38b64f9b84a4e27006af142bf6bf4a3aa2b752ed
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-09-26 17:32:52 -0700

    Added eventlistener to newdiv to change bgcolor on mouseover

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ for (let i = 0; i < 256; i++) {
     newDiv = document.createElement('div');
     newDiv.classList.add('grid');
     container.appendChild(newDiv);
+
+    newDiv.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = "aqua";
+    });
 }
